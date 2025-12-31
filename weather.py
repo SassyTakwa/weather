3fcce5dda17aae8d20a21980b3b1d609232508bf
SassyTakwa <sassyytakwa@gmail.com>
2025-12-31 19:38:15 +0100

    Remove hardcoded API key from weather.py
    Removed hardcoded API key for security reasons.

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 city=input("Enter Your City --> ")
-Api_Key = "a730448f523416f46aa7cfc0c1d72643" # Paste Your API ID Here
+Api_Key = "" # Paste Your API ID Here
 
 final_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city,Api_Key)
 
